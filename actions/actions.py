e2f7177024ad35a378198d66fe7f561497abe66d
Daud Ahmad <73556387+DaudAhmad0303@users.noreply.github.com>
2022-12-10 13:33:27 +0500

    Added new story about night temperature every quest separatly

--- actions/actions.py
+++ actions/actions.py
@@ -118,7 +118,7 @@ class ActionMornTemp(Action):
         # print(whole_JSON, type(whole_JSON))
         
         # Getting desired weather service
-        morn_temp = whole_JSON['daily'][0]['temp']['day']
+        morn_temp = whole_JSON['daily'][0]['temp']['morn']
         
         morn_temp = round(float(morn_temp) - 273.15, 2)
         
@@ -127,6 +127,79 @@ class ActionMornTemp(Action):
             bot_response_to_send = f"{most_matched_day} {most_matched_city} میں صبح کا درجہ حرارت {morn_temp} ڈگری سینٹی گریڈ ہے۔"
         else:
             bot_response_to_send = f"{most_matched_day} {most_matched_city} میں صبح کا درجہ حرارت {morn_temp} ڈگری سینٹی گریڈ رہے گا۔"
+        
+        dispatcher.utter_message(text=bot_response_to_send)
+
+        return [SlotSet("morn_temp", morn_temp)]
+
+class ActionHelloWorld(Action):
+    
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("action_hello_world is called")
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class ActionNightTemp(Action):
+    
+    def name(self) -> Text:
+        return "action_utter_night_temp"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("action_utter_night_temp is called")
+        
+        # getting entity value of `city` and `day`
+        received_city = next(tracker.get_latest_entity_values("city"), "لاہور")
+        received_day = next(tracker.get_latest_entity_values("day"), "آج")
+        
+        print(received_city)
+        
+        # Getting most matched day name with custom function
+        most_matched_day = get_matched_name(received_day, "day")[0]
+        
+        # Getting relative day-number for reteriving data from Database
+        day_no_for_DB = relative_day_no(received_day)
+        print(day_no_for_DB)
+        
+        # Getting most matched city name with custom function
+        most_matched_city = get_matched_name(received_city, "city")[0]
+        
+        # Getting the GeoLocations' Document of `city` from Database
+        document = geoLocationsCollection.find_one({'city': most_matched_city})
+        print(document)
+        
+        latitude = float(document['latitude'])
+        longitude = float(document['longitude'])
+        
+        # Getting Whole JSON/Document from DB of desired city
+        weatherAPIDataCollection = db2[most_matched_city]
+        whole_JSON = weatherAPIDataCollection.find_one(
+            {
+                "lat": latitude,
+                "lon": longitude,
+            }
+        )
+        # print(whole_JSON, type(whole_JSON))
+        
+        # Getting desired weather service
+        morn_temp = whole_JSON['daily'][0]['temp']['night']
+        
+        morn_temp = round(float(morn_temp) - 273.15, 2)
+        
+        # bot_response_to_send = "{} میں زیادہ سے زیادہ درجہ حرارت 20 ڈگری سینٹی گریڈ رہے گا، جبکہ کم سے کم درجہ حرارت {} ڈگری سینٹی گریڈ رہے گا۔"
+        
+        if day_no_for_DB == 0:
+            bot_response_to_send = f"{most_matched_day} {most_matched_city} میں رات کا درجہ حرارت {morn_temp} ڈگری سینٹی گریڈ ہے۔"
+        else:
+            bot_response_to_send = f"{most_matched_day} {most_matched_city} میں رات میں درجہ حرارت {morn_temp} ڈگری سینٹی گریڈ رہے گا۔"
         
         dispatcher.utter_message(text=bot_response_to_send)
 
