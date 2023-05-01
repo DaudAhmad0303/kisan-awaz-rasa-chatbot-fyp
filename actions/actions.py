2fc3f77664341924445e9b14c7bf0c8a00fb8aad
Daud Ahmad <73556387+DaudAhmad0303@users.noreply.github.com>
2023-05-01 21:25:59 +0500

    added action_weather

--- actions/actions.py
+++ actions/actions.py
@@ -33,9 +33,91 @@ print("Database updated on", time_DB_updated())
 
 # allDatabases = client.list_database_names()
 # pprint(allDatabases)
+def get_weather_description(id: int) -> str:
+    """Generates relative weather description based upon id from OpenWeatherMapAPI in Urdu.
 
-"""
-class ActionSetGeoLocation(Action):
+    Args:
+        id (int): ID of the Weather present in OpenWeatherMapAPI
+
+    Returns:
+        str: Description of the weather in Urdu relative to the ID.
+    """
+    weather_description = dict()
+    weather_description = {
+        # Thunderstorm Group
+        200: "گرج چمک کے ساتھ ہلکی بارش",           # Thunderstorm with light rain
+        201: "گرج چمک کے ساتھ بارش",                # Thunderstorm with rain
+        202: "گرج چمک کے ساتھ تیز بارش",            # Thunderstorm with heavy rain
+        210: "ہلکی گرج چمک",                        # Light Thunderstorm
+        211: "گرج چمک",                             # Thunderstorm
+        212: "شدید گرج چمک",                        # Heavy Thunderstorm
+        221: "شدید گرج چمک والی طوفانی بارش",              # Ragged Thunderstorm
+        230: "گرج چمک کے ساتھ ہلکی بوندا باندی",    # Thunderstorm with light drizzle
+        231: "گرج چمک کے ساتھ بوندا باندی",         # Thunderstorn with drizzle
+        232: "گرج چمک کے ساتھ تیز بوندا باندی",     # Thunderstorm with heavy drizzle
+        
+        # Drizzle Group
+        300: "ہلکی ہلکی بوندا باندی",               # Light intensity drizzle
+        301: "بوندا باندی",                         # Drizzle
+        302: "موسلادھار بوندا باندی",                # Heavy intensity drizzle
+        310: "ہلکی شدت کی بوندا باندی",             # Light intensity drizzle rain
+        311: "بوندا باندی",                         # Drizzle rain
+        312: "موسلادھار بوندا باندی",                # Heavy intensity drizzle rain
+        313: "بارش اور بوندا باندی",                # Shower rain and drizzle
+        314: "موسلادھار بارش اور بوندا باندی",       # Heavy shower rain and drizzle
+        321: "بوندا باندی کی بوچھاڑ",               # Shower drizzle
+        
+        # Rain Group
+        500: "بہت ہلکی بوندا باندی",             # Light Rain
+        501: "ہلکی بارش",                        # Moderate Rain
+        502: "شدید بارش",                        # Heavy intensity rain
+        503: "بہت تیز بارش",                     # Very Heavy Rain
+        504: "بہت شدید بارش",                    # Extreme Rain
+        511: "برفیلی بارش",                      # Freezing Rain
+        520: "ہلکی شدت کی بارش",                 # Light intensity Shower
+        521: "بوندا باندی",                      # Shower Rain
+        522: "موسلا دھار بارش",                   # Heavy intensity Shower Rain
+        531: "سیلابی بارش",                       # Ragged Shower Rain
+        
+        # Snow Group
+        600: "ہلکی برف باری",                   # Light snow
+        601: "برفباری",                         # Snow
+        602: "شدید برفباری",                    # Heavy snow
+        611: "ژالہ باری",                       # Sleet
+        612: "ہلکی بوچھاڑ والی ژالہ باری",      # Light shower sleet
+        613: "بوچھاڑ والی ژالہ باری",           # shower sleet
+        615: "ہلکی بارش کے ساتھ برفباری",       # light rain and snow
+        616: "بارش اور برفباری",                # rain and snow
+        620: "ہلکی بوچھاڑ والی برفباری",        # light shower snow
+        621: "بوچھاڑ والی برفباری",             # shower snow
+        622: "شدید بوچھاڑ والی برفباری",        # Heavy shower snow
+        
+        # Atmosphere Group
+        701: "فضا میں ہلکی دھند چھائی",         # Mist
+        711: "فضا میں دھواں دھواں پھیلا",             # Smoke
+        721: "فضا میں گرد و غبار پھیلا",         # Haze
+        731: "فضا میں ریت کے بگولہ چل رہے",     # Sand/Dust whirls
+        741: "فضا میں دھند چھائی",              # Fog
+        751: "فضا میں ریت اڑ رہی",              # Sand
+        761: "فضا میں دھول کا سماں",            # Dust
+        762: "فضا میں آتش فشاں راکھ اڑ رہی",        # Volcanic ash
+        771: "تیز ہواوں کے طوفان چل رہے",       # Squalls
+        781: "تیز آندھی چل رہی",                # Tornado
+        
+        # Clouds Group
+        800: "آسمان صاف",                        # Clear Sky
+        801: "آسمان پر تھوڑے بادل چھائے",        # Few Clouds
+        802: "آسمان پر بکھرے بادل چھائے",        # Scattered Clouds
+        803: "آسمان پر ٹوٹے بادل چھائے",         # Broken Clouds
+        804: "آسمان پر ابر آلود بادل چھائے",     # Overcast Clouds
+    }
+    
+    if id in weather_description:
+        return weather_description[id] 
+    else:
+        return " "
+
+"""class ActionSetGeoLocation(Action):
     
     def name(self) -> Text:
         return "action_set_geo_location"
@@ -620,4 +702,89 @@ class ActionUVindex(Action):
         dispatcher.utter_message(text=bot_response_to_send)
 
         return [SlotSet("uv_index", uv_index)]
+
+class ActionWeather(Action):
+    
+    def name(self) -> Text:
+        return "action_utter_weather"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("action_utter_weather is called")
+        
+        # getting entity value of `city` and `day`
+        received_city = tracker.get_slot("city")
+        received_day = tracker.get_slot("day")
+        
+        print(received_city)
+        print(received_day)
+        
+        # Getting most matched day name with custom function
+        most_matched_day = get_matched_name(received_day, "day")[0]
+        
+        # Getting relative day-number for reteriving data from Database
+        day_no_for_DB = relative_day_no(received_day, DB_update_time = time_DB_updated(formated=False))
+        print(day_no_for_DB)
+        
+        # Getting most matched city name with custom function
+        most_matched_city = get_matched_name(received_city, "city")[0]
+        
+        # Getting the GeoLocations' Document of `city` from Database
+        document = geo_locations_collection.find_one({'city': most_matched_city})
+        print(document)
+        
+        latitude = float(document['latitude'])
+        longitude = float(document['longitude'])
+        
+        # Getting Whole JSON/Document from DB of desired city
+        whole_JSON = weather_forecast_collection.find_one(
+            {
+                "lat": latitude,
+                "lon": longitude
+            }
+        )
+        # print(whole_JSON, type(whole_JSON))
+        
+        # Getting desired weather service
+        weather_id = whole_JSON['daily'][day_no_for_DB]['weather'][0]['id']
+        
+        urdu_weather_description = get_weather_description(weather_id)
+        
+        if day_no_for_DB == 0 or most_matched_day == "آج":
+            if weather_id in range(200, 700):
+                urdu_weather_description = f"{urdu_weather_description} ہو رہی ہے۔"
+            elif weather_id in [701, 741]:
+                urdu_weather_description = f"{urdu_weather_description} ہوئی ہے۔"
+            elif weather_id in [711, 721]:
+                urdu_weather_description = f"{urdu_weather_description} ہوا ہے۔"
+            elif weather_id in [731, 771]:
+                urdu_weather_description = f"{urdu_weather_description} ہیں۔"
+            elif weather_id in [751, 761, 762, 781, 800]:
+                urdu_weather_description = f"{urdu_weather_description} ہے۔"
+            elif weather_id in [801, 802, 803, 804]:
+                urdu_weather_description = f"{urdu_weather_description} ہوئے ہیں۔"
+        else:
+            if weather_id in range(200, 700):
+                urdu_weather_description = f"{urdu_weather_description} ہو رہی ہوگی۔"
+            elif weather_id in [701, 741]:
+                urdu_weather_description = f"{urdu_weather_description} ہوئی ہوگی۔"
+            elif weather_id in [711, 721]:
+                urdu_weather_description = f"{urdu_weather_description} ہوا گا۔"
+            elif weather_id in [731, 771, 801, 802, 803, 804]:
+                urdu_weather_description = f"{urdu_weather_description} ہوں گے۔"
+            elif weather_id in [761, 800]:
+                urdu_weather_description = f"{urdu_weather_description} ہوگا۔"
+            elif weather_id in [751, 762, 781, 800]:
+                urdu_weather_description = f"{urdu_weather_description} ہوگی۔"
+        
+        
+        if day_no_for_DB == 0 or most_matched_day == "آج":
+            bot_response_to_send = f"{most_matched_day} {most_matched_city} میں {urdu_weather_description}"
+        else:
+            bot_response_to_send = f"{most_matched_day} {most_matched_city} میں {urdu_weather_description}"
+        
+        dispatcher.utter_message(text=bot_response_to_send)
+
+        return [SlotSet("weather_id", weather_id)]
 
