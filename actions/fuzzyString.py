19a52515c98465406a2f3b4e84d6ade3d9bea631
Daud Ahmad <73556387+DaudAhmad0303@users.noreply.github.com>
2023-05-11 17:23:34 +0500

    Added and moved Lists for fuzzystring in actions dir
    Added lists of fertilizers, pesticides, machinery in data_for_fuzzy_string directory and moved CitiesList, CitiesList490 and DaysList to the same directory

--- actions/fuzzyString.py
+++ actions/fuzzyString.py
@@ -18,15 +18,30 @@ def get_matched_name(name :str, name_type: str, data = list()):
         _type_: `None` if the matching proportion is less than 60,
                     Otherwise, the `str` name with the maximum matching ratio.
     """
-
+    data = list()
     if name_type == "city":
         # Reading the Cities files as list
-        with open("CitiesList490.txt", encoding='UTF-8') as file:
+        with open("data_for_fuzzy_string\CitiesList490.txt", encoding='UTF-8') as file:
             for val in file.readlines():
                 data.append(val.strip())
-    else:
+    elif name_type == "day":
         # Reading the Days files as list
-        with open("DaysList.txt", encoding='UTF-8') as file:
+        with open("data_for_fuzzy_string\DaysList.txt", encoding='UTF-8') as file:
+            for val in file.readlines():
+                data.append(val.strip())
+    elif name_type == "fertilizer":
+        # Reading the Cities files as list
+        with open("data_for_fuzzy_string\FertilizersList.txt", encoding='UTF-8') as file:
+            for val in file.readlines():
+                data.append(val.strip())
+    elif name_type == "pesticide":
+        # Reading the Cities files as list
+        with open("data_for_fuzzy_string\PesticidesLists.txt", encoding='UTF-8') as file:
+            for val in file.readlines():
+                data.append(val.strip())
+    elif name_type == "machinery":
+        # Reading the Cities files as list
+        with open("data_for_fuzzy_string\MachineryList.txt", encoding='UTF-8') as file:
             for val in file.readlines():
                 data.append(val.strip())
     
@@ -46,8 +61,10 @@ def get_matched_name(name :str, name_type: str, data = list()):
 
 
 if __name__ == "__main__":
-    print(get_matched_name("سومو", "day"))
-    print(get_matched_name("passport", "day"))
+    print(get_matched_name("لاہو", "city"))
+    print(get_matched_name("سوگوار", "day"))
+    print(get_matched_name("سونا", "fertilizer"))
+    print(get_matched_name("ٹرک", "machinery"))
 
     # str_list = ['Joe Biden', 'Joseph Biden', 'Joseph R Biden']
 
